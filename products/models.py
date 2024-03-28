f81e389fa64d898d1f34a0f152fdb4247a2080d9
Erik <erik-guldbrand@hotmail.com>
2024-03-28 14:07:13 +0100

    Product, category models
    Add placeholder product and category models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+    name = models.CharField(max_length=100)
+    friendly_name = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    category = models.ForeignKey(
+        'Category', null=True, blank=False, on_delete=models.CASCADE, related_name="product_category")
+    description = models.TextField(max_length=1000, blank=True)
+    volume = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10000)], blank=False) #Volume in ml
+    price = models.DecimalField(max_digits=7, decimal_places=2, blank=False) #Price in sek
+
+    def __str__(self):
+        return self.name
--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
+from .models import Product, Category
 
 class ProductList(generic.ListView):
+    queryset = Product.objects.all()
     template_name = "products/products.html"
