105a29b62d850f7971f45ac3132754f7a4e9bd10
Erik <erik-guldbrand@hotmail.com>
2024-04-08 14:55:46 +0200

    Sorting function
    Add backend and frontend for sorting functions for products

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category, Subcategory
+from django.db.models.functions import Lower
 
 def products_list(request):
     """ Shows all products and handles sorting and search queries """
@@ -14,6 +15,20 @@ def products_list(request):
     direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split()
             products = products.filter(category__name__in=categories)
@@ -33,10 +48,13 @@ def products_list(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         "products": products,
         "search_term": query,
         "categories": categories,
+        "current_sorting": current_sorting,
     }
 
     return render(request, "products/products.html", context)
