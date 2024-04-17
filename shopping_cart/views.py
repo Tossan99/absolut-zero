32167e3d23da4087fc243fe2a7b676e0ab4afe6f
Erik <erik-guldbrand@hotmail.com>
2024-04-17 16:37:59 +0200

    Toasts
    Implement toasts and design front end for them

--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from products.models import Product
 
 
 def view_shopping_cart(request):
@@ -7,14 +9,17 @@ def view_shopping_cart(request):
 def view_add_to_shopping_cart(request, item_id):
     """Adds items to the shopping cart"""
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get("quantity"))
     redirect_url = request.POST.get("redirect_url")
     shopping_cart = request.session.get("shopping_cart", {})
 
     if item_id in list(shopping_cart.keys()):
         shopping_cart[item_id] += quantity
+        messages.success(request, f"Added {quantity} {product} to your shopping cart.")
     else:
         shopping_cart[item_id] = quantity
+        messages.success(request, f"Added {quantity} {product} to your shopping cart.")
 
     request.session["shopping_cart"] = shopping_cart
     return redirect(redirect_url)
@@ -22,13 +27,16 @@ def view_add_to_shopping_cart(request, item_id):
 def view_adjust_shopping_cart(request, item_id):
     """Changes the quantity of products to the specified amount"""
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get("quantity"))
     shopping_cart = request.session.get("shopping_cart", {})
 
     if quantity > 0:
         shopping_cart[item_id] = quantity
+        messages.success(request, f"Changed the amount of {product} to {quantity}.")
     else:
         shopping_cart.pop(item_id)
+        messages.success(request, f"Changed the amount of {product} to {quantity}.")
 
     request.session["shopping_cart"] = shopping_cart
     return redirect(reverse("shopping_cart"))
@@ -36,8 +44,10 @@ def view_adjust_shopping_cart(request, item_id):
 def view_remove_from_shopping_cart(request, item_id):
     """Removes items from shopping cart"""
 
+    product = Product.objects.get(pk=item_id)
     shopping_cart = request.session.get("shopping_cart", {})
     shopping_cart.pop(item_id)
     request.session["shopping_cart"] = shopping_cart 
+    messages.success(request, f"Removed all {product} from shopping cart.")
     return redirect(reverse("shopping_cart"))
    
