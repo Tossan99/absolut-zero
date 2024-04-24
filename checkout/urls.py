3b720407454571f0b19cfdc6f53b9edb047907f2
Erik <erik-guldbrand@hotmail.com>
2024-04-25 01:16:31 +0200

    Stripe webhooks
    Complete  webhook handler

--- checkout/urls.py
+++ checkout/urls.py
@@ -6,5 +6,6 @@ from .webhooks import webhook
 urlpatterns = [
     path("", views.view_checkout, name="checkout"),
     path("checkout_success/<order_number>", views.view_checkout_success, name="checkout_success"),
+    path("cache_checkout_data/", views.view_cache_checkout_data, name="cache_checkout_data"),
     path("wh/", webhook, name="webhook"),
 ]
