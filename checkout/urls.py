552934d1a56cc2923c44384a97b9a8d1e6adb7f4
Erik <erik-guldbrand@hotmail.com>
2024-04-25 00:02:14 +0200

    Stripe webhooks
    Add class methods to webhook handler and webhook view

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
+
 
 urlpatterns = [
     path("", views.view_checkout, name="checkout"),
     path("checkout_success/<order_number>", views.view_checkout_success, name="checkout_success"),
+    path("wh/", webhook, name="webhook"),
 ]
