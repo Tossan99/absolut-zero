a2cd243ced2d5ff9f81c1e647d1a48e1cfa959c4
Erik <erik-guldbrand@hotmail.com>
2024-04-25 21:59:49 +0200

    Profile
    Design the profile page and add order history to it. Add webhook function to submit profile information form if needed

--- checkout/views.py
+++ checkout/views.py
@@ -5,7 +5,10 @@ from django.conf import settings
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
+
 from products.models import Product
+from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
 from shopping_cart.contexts import shopping_cart_contents
 
 import stripe
@@ -114,6 +117,28 @@ def view_checkout_success(request, order_number):
     """
     save_info = request.session.get("save_info")
     order = get_object_or_404(Order, order_number=order_number)
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user"s profile to the order
+        order.user_profile = profile
+        order.save()
+
+        # Save the user"s info
+        if save_info:
+            profile_data = {
+                "default_phone_number": order.phone_number,
+                "default_country": order.country,
+                "default_postcode": order.postcode,
+                "default_town_or_city": order.town_or_city,
+                "default_street_address1": order.street_address1,
+                "default_street_address2": order.street_address2,
+                "default_county": order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
     messages.success(request, f"Order successfully processed! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.")
@@ -121,9 +146,8 @@ def view_checkout_success(request, order_number):
     if "shopping_cart" in request.session:
         del request.session["shopping_cart"]
 
-    template = "checkout/checkout_success.html"
     context = {
         "order": order,
     }
 
-    return render(request, template, context)
+    return render(request, "checkout/checkout_success.html", context)
--- profiles/urls.py
+++ profiles/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.view_profile, name="profile"),
+    path("order_history/<order_number>", views.view_order_history, name="order_history"),
     
 ]
