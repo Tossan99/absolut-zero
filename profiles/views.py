c62057252605fd68836e18720207b122ba05bc02
Erik <erik-guldbrand@hotmail.com>
2024-04-25 20:43:17 +0200

    Allauth templates
    Add and design allauth templates and create a placeholder profile page

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import UserProfile
+
+
 def view_profile(request):
-    return render (request, "profiles/profile.html")
+    """ Display the user's profile. """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    context = {
+        "profile": profile,
+    }
+
+    return render(request, "profiles/profile.html", context)
