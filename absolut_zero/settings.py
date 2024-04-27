0be5e3a71209ba9bcfb34e2d32b42acb0b237705
Erik <erik-guldbrand@hotmail.com>
2024-04-27 11:43:24 +0200

    Homepage
    Change width on hero image logo

--- absolut_zero/settings.py
+++ absolut_zero/settings.py
@@ -216,11 +216,10 @@ STRIPE_CURRENCY = "sek"
 STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY", "")
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY", "")
 STRIPE_WH_SECRET = os.getenv("STRIPE_WH_SECRET", "")
-DEFAULT_FROM_EMAIL = "absolut_zero@example.com"
 
 if "DEVELOPMENT" in os.environ:
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-    DEFAULT_FROM_EMAIL = "absolutzero@example.com"
+    DEFAULT_FROM_EMAIL = "absolutzero987@example.com"
 else:
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_USE_TLS = True
